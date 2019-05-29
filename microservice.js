637000a71c4d137543f2d5f492641fb24b48afdf
SalvadorBlanco <jose.blanco@ugto.mx>
2019-05-29 13:18:42 -0500

    Installing Nodes 'Express' and 'MySQL', the code from microservice.js was modified to use POST

--- microservice.js
+++ microservice.js
@@ -1,14 +1,49 @@
-const http = require('http');
+var express = require("express");
+var app=express();
+var usuario;
+const mysql = require('mysql');
 
-const hostname = 'localhost';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+//Se crea la coneccion.
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'salvador',
+  password: 'Chavapi181113<3',
+  database: 'clients'
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+var bodyParser=require("body-parser")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/login',function(req,res,next){
+  if(req.query.filter){
+      next();
+      return;
+  }
+  res.send('Login.');
 });
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+
+//POST
+app.post('/plogin',function(req,res)
+{
+    var user_n =req.body.id;
+    var pass=req.body.pass;
+    var Aut;
+    connection.query("SELECT count(*) FROM clients WHERE name=" + mysql.escape(user_n) + "AND password=" +mysql.escape(pass),function(err,result,fields)
+    {
+       if(err) throw err;
+       console.log("User Login") 
+       Aut=result;
+    });
+});
+
+var server = app.listen(8080,function(){
+  console.log('Service Running...');
+});
+
+
